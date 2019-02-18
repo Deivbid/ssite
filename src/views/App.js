acb551f0fff9d1cdad12ffe08aaa6d10d10dbcec
David <davidaapariciobak@gmail.com>
2019-02-18 08:02:32 -0300

    first commit

---
+++ src/views/App.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          This is going to be the app
+        </header>
+      </div>
+    );
+  }
+}
+
+export default App;
